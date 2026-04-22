1f95b8a20c2069d169b520cc46015d20e1fa9c92
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-04-22 15:09:45 -0300

    enviando o projeto

---
+++ app.js
@@ -0,0 +1,42 @@
+//  NÃO FUNCIONOU
+
+
+//Import das dependencias para criar a API
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require('body-parser')
+const controllerFilme = require('./controller/filme/controller_filme.js')
+// Import das controllers do projeto
+
+//Permitindo a utilização do JSON no body das requisições
+const bodyParserJson = bodyParser.json()
+
+//Criando um objeto do express para criar a API
+const app = express()
+
+// Configurações do cors da API
+const corsOption = {
+    origin: ["*"],
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: ["Content-type", "Authorization"],
+}
+
+app.use(cors(corsOption))
+
+//ENDPOINTS
+app.post('/v1/senai/locadora/filme',bodyParserJson, async function (request, response){
+    // npm install body-parser  --save para receber o que o front mandar
+
+    // Recebendo o body da requisição
+    let dados = request.body
+
+    let result = await controllerFilme.inserirNovoFilme(dados)
+
+    response.status(result.status_code)
+    response.json(result)
+})
+
+//Fazer o Start na API (aguardando as requisições)
+app.listen(8080, function(){
+    console.log("API aguardando novas requisições . . . .")
+})
